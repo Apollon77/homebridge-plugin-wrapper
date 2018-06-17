3a61ed9d2c1055dc9148f75f7e0be1e1a7573a1f
Ingo Fischer <github@fischer-ka.de>
2018-06-18 00:06:49 +0200

    change version checks

--- homebridge/version.js
+++ homebridge/version.js
@@ -6,7 +6,5 @@ var path = require('path');
 module.exports = getVersion();
 
 function getVersion() {
-  var packageJSONPath = path.join(__dirname, '../package.json');
-  var packageJSON = JSON.parse(fs.readFileSync(packageJSONPath));
-  return packageJSON.version;
+  return '0.4.43';
 }
